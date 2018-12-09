6bc9517575aa1f97e3ffc4744f940d738db50807
Jose Rivas <jose.j.rivasgarcia@gmail.com>
2018-12-09 13:31:52 -0600

    lambda done

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -3,23 +3,31 @@ from botocore.client import Config
 from io import BytesIO
 import zipfile
 import os
-
-s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
+import mimetypes
 
 def lambda_handler(event, context):
-    portfolio_bucket = s3.Bucket('portfolio.rotios.io')
-    build_bucket = s3.Bucket('portfoliobuild.rotios.io')
-    
-    portfolio_zip = BytesIO()
-    object_to_download = build_bucket.Object('BuildPortfolio.zip')
-    object_to_download.download_fileobj(portfolio_zip)
+    try:
+        s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
+        sns = boto3.resource('sns')
+        topic = sns.Topic('arn:aws:sns:us-east-1:504110333788:deployPortfolioTopic')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket = s3.Bucket('portfolio.rotios.io')
+        build_bucket = s3.Bucket('portfoliobuild.rotios.io')
+        
+        portfolio_zip = BytesIO()
+        object_to_download = build_bucket.Object('BuildPortfolio.zip')
+        object_to_download.download_fileobj(portfolio_zip)
 
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-if __name__ == '__main__':
-    lambda_handler(None, None)
+        topic.publish(Subject="Rotios IO Portfolio built successfully", Message="The build was successful.")        
+    except Exception as e:
+        topic.publish(Subject="Rotios IO Portfolio build Failed", Message="The build failed with error:\n {}.".format(e))
+        raise e
+
+# if __name__ == '__main__':
+#     lambda_handler(None, None)
