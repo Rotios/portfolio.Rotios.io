8d1cf0f597a9708b789cbcc7e034cd9ac31cdac2
Jose Rivas <jose.j.rivasgarcia@gmail.com>
2018-12-09 13:03:54 -0600

    added upload portfolio lambda python

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -1,7 +1,8 @@
 import boto3
 from botocore.client import Config
-import StringIO
+from io import BytesIO
 import zipfile
+import os
 
 s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
 
@@ -9,9 +10,16 @@ def lambda_handler(event, context):
     portfolio_bucket = s3.Bucket('portfolio.rotios.io')
     build_bucket = s3.Bucket('portfoliobuild.rotios.io')
     
-    import os
-    path = os.getcwd() + 'portfoliobuild.zip'
-    build_bucket.download_file('portfoliobuild.zip', path)
+    portfolio_zip = BytesIO()
+    object_to_download = build_bucket.Object('BuildPortfolio.zip')
+    object_to_download.download_fileobj(portfolio_zip)
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_file()
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
