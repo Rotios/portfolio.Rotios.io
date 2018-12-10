ef5640ff1f25b98e22c88a6bfa399246e41d3f46
ose Rivas <jose.j.rivasgarcia@gmail.com>
2018-12-09 22:17:51 -0600

    added example stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work'
 
-console.log("Loaded react-dom")
+console.log("test")
+
+const myWork = [
+    {
+        'title':"Work Example",
+        'image' : {
+            "desc" : "Example",
+            'src' : 'images/example1.png',
+            'comment': ''
+        }
+    },
+    {
+        'title':"Work Example",
+        'image' : {
+            "desc" : "Example",
+            'src' : 'images/example2.png',
+            'comment': ''
+        }
+    },
+    {
+        'title':"Work Example",
+        'image' : {
+            "desc" : "Example",
+            'src' : 'images/example3.png',
+            'comment': ''
+        }
+    }
+]
+
+ReactDOM.render(<ExampleWork work = {myWork} />, document.getElementById("example-work"))
