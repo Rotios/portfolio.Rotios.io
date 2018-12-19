e88f7eb285e13908869c423f609e17c43ff8d883
ose Rivas <jose.j.rivasgarcia@gmail.com>
2018-12-18 23:28:28 -0600

    Adding project for tonys adventure

---
+++ js/projects/tonys-adventure.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class TonysAdventure extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return (
+        <span>
+            <section className="section section--alignCentered section--description">
+                <p>COMING SOON</p>
+            </section>
+        </span>
+        )
+    }
+}
+
+export default TonysAdventure
