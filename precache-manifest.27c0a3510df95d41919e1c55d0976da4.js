ec880ce8fc9c9ccb14533362fed2fc781eb42554
Jose Rivas <jose.j.rivasgarcia@gmail.com>
2019-06-29 23:18:22 -0500

    Updates

---
+++ precache-manifest.27c0a3510df95d41919e1c55d0976da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3b3cf75408010fe5743bcc4b19eb55",
+    "url": "/static/media/background.3e3b3cf7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c54415f16220ae395d6c",
+    "url": "/static/js/main.c54415f1.chunk.js"
+  },
+  {
+    "revision": "b418196f8b9d580d6a25",
+    "url": "/static/js/1.b418196f.chunk.js"
+  },
+  {
+    "revision": "c54415f16220ae395d6c",
+    "url": "/static/css/main.9281afb8.chunk.css"
+  },
+  {
+    "revision": "ec0fe6df8ee845867c880993c5b27e1c",
+    "url": "/index.html"
+  }
+];
